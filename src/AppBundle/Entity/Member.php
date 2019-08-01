18500c34545952ce0508b498437849595785b741
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-08-01 18:24:57 +0200

    fakeadmin login successfull

--- src/AppBundle/Entity/Member.php
+++ src/AppBundle/Entity/Member.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Member
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="member")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\MemberRepository")
  */
-class Member
+class Member implements UserInterface, \Serializable
 {
     /**
      * @var int
